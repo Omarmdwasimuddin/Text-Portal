630777564cc91a9b490e50ad7231ce65fad629a5
Muhammad Wasim Uddin Omar <mdwasimu015@gmail.com>
2025-11-04 09:51:06 +0600

    Inshallah

--- src/component/acts/Master.jsx
+++ src/component/acts/Master.jsx
@@ -1,11 +1,9 @@
 import Navbar from '@/component/master/Navbar';
 import Footer from '@/component/master/Footer';
-import Banner from '@/component/master/Banner';
 
 const Master = ({ children }) => {
   return (
     <div>
-      <Banner/>
       <Navbar/>
       {children}
       <Footer/>
