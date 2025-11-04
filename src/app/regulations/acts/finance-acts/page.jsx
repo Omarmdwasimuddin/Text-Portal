bda5aec333f7ef491047beb9f32ba55297a4f91e
Muhammad Wasim Uddin Omar <mdwasimu015@gmail.com>
2025-11-04 09:40:14 +0600

    Inshallah

---
+++ src/app/regulations/acts/finance-acts/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FinanceActs from '@/component/acts/FinanceActs';
+
+const page = () => {
+    return (
+        <div>
+            <FinanceActs/>
+        </div>
+    );
+};
+
+export default page;
