ee0a190ea82baa97f969edbcd06cce1a96130dfd
Muhammad Wasim Uddin Omar <mdwasimu015@gmail.com>
2025-11-11 15:45:28 +0600

    Inshallah

---
+++ src/app/laws/law1991/section/section07/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Section07 from '@/component/section1991/Section07';
+import Master2 from '@/component/laws/Master2';
+
+const page = () => {
+    return (
+        <Master2>
+            <Section07 />
+        </Master2>
+    );
+};
+
+export default page;
