57fcdc0497c8b1981dbe4f4fda360a7f9e8452b6
Md Wasim Uddin <mdwasimu015@gmail.com>
2026-01-02 18:38:04 +0600

    Inshallah

---
+++ src/app/laws/law2012/section/section139/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Section31 from '@/component/section2012/Section31';
+import Master from '@/component/laws/Master';
+
+const page = () => {
+    return (
+        <Master>
+            <Section31 />
+        </Master>
+    );
+};
+
+export default page;
