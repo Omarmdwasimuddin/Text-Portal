f8d4370926b9eb9c50e3bbf054bf6c24f8e6b6d0
Md Wasim Uddin <mdwasimu015@gmail.com>
2026-01-13 12:33:15 +0600

    page create

---
+++ src/app/laws/customsacts2023/section/section97/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Section03 from '@/component/customsacts2023/Section03';
+import Master3 from '@/component/laws/Master3';
+
+
+const page = () => {
+    return (
+        <Master3>
+            <Section03 />
+        </Master3>
+    );
+};
+
+export default page;
