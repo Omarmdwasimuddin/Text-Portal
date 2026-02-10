03a86a16e8ebb4a0707a2b0883b07a2c62b1f932
Md Wasim Uddin <mdwasimu015@gmail.com>
2026-02-10 15:38:21 +0600

    err fix

--- src/app/laws/excisessaltact1944/section/section19/page.jsx
+++ src/app/laws/excisessaltact1944/section/section19/page.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Section1901 from '@/component/excisessaltact1944/Section1901';
+import Section19 from '@/component/excisessaltact1944/Section19';
 import Master2 from '@/component/laws/Master2';
 
 const page = () => {
     return (
         <Master2>
-            <Section1901 />
+            <Section19 />
         </Master2>
     );
 };
